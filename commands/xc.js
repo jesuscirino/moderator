0e540c03e14cef5ffe5e5dfc3610c7b409a6f29f
jesuscirino <jesuscirino@gmail.com>
2018-05-21 07:24:06 -0500

    cuidado borra todo!

--- commands/xc.js
+++ commands/xc.js
@@ -6,13 +6,15 @@ module.exports = {
     async execute(client, message, args) {
         if(!args || args.length < 2) return message
             .reply(`${mark}Debes colocar un número de días y el ID del canal ${down}`) ;
-        const [numOfdays, idChannel] = args
+        const [idChannel, numOfdays ] = args
         const embed = Object.assign({}, embedZero)
         const cervantes = client.guilds.get(ID_CERVANTES)
         const ch = cervantes.channels.get(idChannel)
-        //embed.title = `seleccionado ${ch.name} `
-        const sentMessage1 = await message.channel.send(`me tardaré algunos minutos 😅`);
-        const counter = await deleteAllUntilAgo(numOfdays, ch)
-        const sentMessage = await message.channel.send(`se borraron ${counter}`, {embed})
+        const counter = await deleteAllUntilAgo(numOfdays, ch, false)
+        embed.title = `seleccionado ${ch.name} `
+        const sentMessage1 = await message.channel.send(`Espera ... 😅`);
+        embed.description = `${mark} Borré  ${counter} mensajes!  ${down}`;
+        const sentMessage = message.channel.send("", {embed});
+        //const sentMessage = await message.channel.send(`se borraron ${counter}`, {embed})
         }
 }
